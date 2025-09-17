c83cf8e213246e183359c0259ee4647fcce351d9
Darshan Vasa <vdarshan786@gmail.com>
2025-09-17 22:35:15 +0530

    Problem Dependency Injection

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response, Express } from 'express';
+import { Page } from './src/page';
+import { Post } from './src/post';
+import { User } from './src/user';
 
 const app: Express = express();
 const port = 3001;
@@ -7,6 +10,23 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express application");
 });
 
+app.post("/create-post", (req: Request, res: Response) => {
+  let post = new Post('New Post', 'Post Content', new User('John'));
+  res.send('Post Created');
+});
+
+app.post("/create-page", (req: Request, res: Response) => {
+  let post = new Page('http://page.com', new User('John'));
+  res.send('Page Created');
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+/**
+ * Advantages of Dependency Injection
+ * 1. DECOUPLING: DI helps in decoupling components and layers in an application
+ * 2. Ease of Testing: With DI becomes straightforward to test classes by mocking their dependency
+ * 3. Reusablity: Components & services designed to be injected can be reused across different parts of application.
+ */
